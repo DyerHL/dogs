7347ea63ee78c7ecb56c176684f5079ed1e965b7
Halie Dyer <halie.l.dyer@gmail.com>
2022-01-10 21:20:12 -0500

    add it all

---
+++ ClassesNamespaces/Pets/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassesNamespaces.Pets
+{
+    //class - a custom type (Dog is a custom type) - classes get title case
+    public class Dog
+    {   //properties (auto property- has a plain getter/setter) (calculated property - )
+        //access modifier (ex. public, private) Public means that this property can be accessed outstide of this class, Structure: type  name  getter & setter
+        public string FurType { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public double Weight { get; set; }
+        public DateTime DateOfBirth { get; set; } //can have private setter
+        //calculated property
+        public int Age
+        {
+            get
+            {
+                return (DateTime.Now - DateOfBirth).Days / 365;
+            }
+        }
+
+
+        //methods  - TitleCase, talks about the behaviors of a class
+        //access modifier  Structure: return type  name  code
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} barks loudly.");
+        }
+
+    }
+}
---
+++ ClassesNamespaces/Program.cs
@@ -0,0 +1,34 @@
+﻿using ClassesNamespaces.Pets;
+using System;
+
+namespace ClassesNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //namespaces - a way of organizing code
+
+            var dog1 = new Dog();
+
+            dog1.FurType = "short";
+            dog1.Name = "Fido";
+            dog1.Breed = "pitbull";
+            dog1.Weight = 74.5;
+
+            Console.WriteLine($"{dog1.Name} is a {dog1.Breed} with {dog1.FurType} fur and weighs {dog1.Weight}");
+
+            Dog dog2 = new Dog();
+            dog2.FurType = "long";
+            dog2.Name = "Astro";
+            dog2.Breed = "Husky";
+            dog2.Weight = 1.2;
+
+            dog2.Bark();
+            dog1.Bark();
+            dog1.DateOfBirth = DateTime.Now.AddDays(-1000);
+            Console.WriteLine($"{dog1.Name} is {dog1.Age} years old.");
+
+        }
+    }
+}
